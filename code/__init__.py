6adba1a6352f367f40b7effbe5ca2e6eecccedd0
pseeth <prem@u.northwestern.edu>
2019-04-18 13:55:11 -0500

    fix for determinism

--- code/__init__.py
+++ code/__init__.py
@@ -3,3 +3,7 @@ from .perceptron import Perceptron, transform_data
 from .polynomial_regression import PolynomialRegression
 from .generate_regression_data import generate_regression_data
 from .load_json_data import load_json_data
+
+# Fix for determinism.
+import numpy as np 
+np.random.seed(0)
