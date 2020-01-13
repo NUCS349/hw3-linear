aed8667469b2dc37c1e13c8af0ca315f2ce046f9
bongjun <bongjun@u.northwestern.edu>
2020-01-12 21:58:54 -0600

    added comments

--- src/metrics.py
+++ src/metrics.py
@@ -8,7 +8,7 @@ def mean_squared_error(estimates, targets):
 
     MSE = (1 / n) * \sum_{i=1}^{n} (Y_i - Yhat_i)^2
 
-    Implement this formula here, using numpy.
+    Implement this formula here, using numpy and return the computed MSE
 
     https://en.wikipedia.org/wiki/Mean_squared_error
     """
